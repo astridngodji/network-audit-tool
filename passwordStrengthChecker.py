c0e7104a58d916aa036260bdd656f09d7189258d
astridngodji <astridngodji@gmail.com>
2026-04-28 22:23:07 +0100

    Add password strength checker and update port scanner

---
+++ passwordStrengthChecker.py
@@ -0,0 +1,52 @@
+import argparse
+
+#checks the lenght of password
+def checkLength(password):
+    if len(password) < 8:
+        return "Minimum of 8 characters"
+
+#checks if there is an uppercase letter
+def checkUppercase(password):
+    if not any(char.isupper() for char in password):
+        return "At least one Uppercase letter"
+
+#check if there is a number
+def checkNumber(password):
+    if not any(char.isdigit() for char in password):
+        return "At least one number"
+
+#check if there is a special character
+def checkSpecialChar(password):
+    special_chars = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~"
+    if not any(char in special_chars for char in password):
+        return "At least one Special Character"
+
+
+for check in [checkLength, checkUppercase, checkNumber, checkSpecialChar]:
+    result = check(password)
+    if result:
+        print(result)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Python Password Strength Checker")
+    parser.add_argument("password", help="Type a password")
+    args = parser.parse_args()
+
+    password = args.password
+
+    errors = []
+    for check in [checkLength, checkUppercase, checkNumber, checkSpecialChar]:
+        result = check(password)
+        if result:
+            errors,append(result)
+    if errors:
+        print("Password is weak:")
+        for err in errors:
+            print(f"- {err}")
+    else:
+        print("Password is strong")
+    
+
+if __name__ == "__main__":
+    main()
--- portScanner.py
+++ portScanner.py
@@ -1,8 +1,13 @@
 import socket
 import argparse
 import time
+import threading
 from concurrent.futures import ThreadPoolExecutor
 
+
+total_ports = 0
+scanned_ports = 0
+lock = threading.Lock()
 
 services = {
                 21: "FTP",
@@ -19,16 +24,23 @@ def scan_port(target, port):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:#(uses IPv4, uses TGP connection)
             s.settimeout(1)  # Set timeout for faster scanning
 
-        # Try connecting to the port
-        if s.connect_ex((target, port)) ==0:
-            service = services.get(port, "Unknown")
-            print(f"[+] Port {port} is OPEN - {service}")
+            # Try connecting to the port
+            if s.connect_ex((target, port)) ==0:
+                service = services.get(port, "Unknown")
+                print(f"[+] Port {port} is OPEN - {service}")
 
     except socket.error:
         pass
-    
+
+    with lock:
+        scanned_ports += 1
+        
 def scan_ports(target, start_port, end_port):
     """Scans a range of ports on the target machine."""
+    global scanned_ports, total_ports
+
+    total_ports = end_port - start_port + 1
+
     print(f"\n[Scanning {target} from port {start_port} to {end_port}]\n")
 
     rate = 50 # scans per sec
@@ -39,6 +51,13 @@ def scan_ports(target, start_port, end_port):
             executor.submit(scan_port, target, port)
             time.sleep(delay) # rate limiting
 
+        while scanned_ports < total_ports:
+            print(f"Progress: {scanned_ports}/{total_ports}", end="\r", flush=True)
+            time.sleep(0.1)
+
+    ##print(f"Progress: {scanned_ports}/{total_ports}", end="\r", flush=True)
+    ##print("Scan complete.")
+    
 def main():
     parser = argparse.ArgumentParser(description="Python Port Scanner")
     parser.add_argument("target", help="Target IP or domain")
